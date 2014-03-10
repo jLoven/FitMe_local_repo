d3069eed366bcd8c693b3f3cdfc31f6bc7e85813
Jackie Loven <jloven@wpi.edu>
2014-03-10 15:25:42 -0400

    FitMe logo, beginning of Splash Screen

---
+++ FitMe/src/com/jackieloven001/fitme/SplashScreen.java
@@ -0,0 +1,21 @@
+package com.jackieloven001.fitme;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SplashScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+	}
+
+}
